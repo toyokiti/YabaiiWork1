e0e8484bae98f341d0f0225fc171e5ad718104ab
toyoda.akihiro <akiakiakihiro0525@icloud.com>
2022-02-03 11:09:36 +0900

    firstHandOver

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 
+// 数字と数字の差分の1サイクルを配列に格納
+let arrCycle = [2, 1, 3, 1, 2, 3];
+// FizzとBuzzの1サイクルを配列に格納
+let arrFizzBuzz = ['Buzz', 'Fizz', 'Fizz', 'Buzz', 'Fizz', 'FizzBuzz'];
+let outputValue = 0;
+// Fizz~FizzBuzzを1サイクルとしてループを回す回数
+let LoopTimes = 7;
 
+for (let i = 0; i < LoopTimes; i++){
 
-
-
+    outputValue += 3;
+    console.log('Fizz ' + outputValue);
+    
+    for (let j = 0; j < arrCycle.length; j++) {
+        outputValue += arrCycle[j];
+        if (outputValue <= 99) {
+            console.log(arrFizzBuzz[j] +' '+ outputValue);
+        }
+    }
+}
