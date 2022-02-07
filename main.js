30cb403cd21457a49ad9d420afa1867debae2ca6
toyoda.akihiro <akiakiakihiro0525@icloud.com>
2022-02-07 10:21:24 +0900

    ロジック修正2

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
 // Fizz＆Buzzの初期値を格納する定数
 const MultipleFizz = 3;
 const MultipleBuzz = 5;
-// Fizz＆Buzzに何回倍数をかけたかを管理する変数
-let FizzMultipleCount = 1;
-let BuzzMultipleCount = 1;
 
-// Fizz＆Buzzぞれぞれの値を管理する変数
-let outputValueFizz = 0;
-let outputValueBuzz = 0;
-
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i < 100; i++) {
     
-    // Fizz＆Buzzの値を計算
-    outputValueFizz = MultipleFizz * FizzMultipleCount;
-    outputValueBuzz = MultipleBuzz * BuzzMultipleCount;
-
-    if(outputValueFizz >= 100) {
-        // Fizzが100以上でforを抜ける(Fizzの方が値が小さいためFizzで判定)
-        break;
+    // Fizz＆Buzzの倍数のとき
+    if(i % MultipleFizz === 0 && i % MultipleBuzz === 0) {
+        console.log(`FizzBuzz ${i}`);
     }
     
-
-    if (outputValueFizz === outputValueBuzz) {
-         if (outputValueFizz < 100) {
-            // 値が100より小さいとき出力
-            console.log(`FizzBuzz ${outputValueFizz}`);
-            FizzMultipleCount++;
-            BuzzMultipleCount++;
-        }   
-    } else if (outputValueBuzz - outputValueFizz >= 3  ) {
-        // FizzとBuzzの差が3より大きい時は、Fizzのみ出力
-        if (outputValueFizz < 100) {
-            // 値が100より小さいとき出力
-            console.log(`Fizz ${outputValueFizz}`);
-            FizzMultipleCount++;
-        }
-    } else {
-        if (outputValueFizz < 100) {
-            // 値が100より小さいとき出力
-            console.log(`Fizz ${outputValueFizz}`);
-            FizzMultipleCount++;
-        }
-        if (outputValueBuzz < 100) {
-            // 値が100より小さいとき出力
-            console.log(`Buzz ${outputValueBuzz}`);
-            BuzzMultipleCount++;
-        }
-    }
+    // Fizzの倍数のとき
+    if(i % MultipleFizz === 0) {
+        console.log(`Fizz ${i}`);
+    } 
+    
+    // Buzzの倍数のとき
+    if(i % MultipleBuzz ===0) {
+        console.log(`Buzz ${i}`);
+    } 
 }
